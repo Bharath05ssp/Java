9fb12e657277bd5d27c380772fccc4a47a0efe08
S BHARATH <141322335+Bharath05ssp@users.noreply.github.com>
2025-08-20 19:12:13 +0530

    Add files via upload

---
+++ Data_types.java
@@ -0,0 +1,19 @@
+class Datatypes{
+    public static void main(String[] args) {
+        // Integer data type
+        int integerValue = 42;
+        System.out.println("Integer Value: " + integerValue);
+
+        // Double data type
+        double doubleValue = 3.14;
+        System.out.println("Double Value: " + doubleValue);
+
+        // Character data type
+        char charValue = 'A';
+        System.out.println("Character Value: " + charValue);
+
+        // Boolean data type
+        boolean booleanValue = true;
+        System.out.println("Boolean Value: " + booleanValue);
+    }
+}
