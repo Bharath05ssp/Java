049a58bbec357aca7c403ee034fd690ffe4e7bbd
S BHARATH <141322335+Bharath05ssp@users.noreply.github.com>
2025-08-20 19:21:06 +0530

    Update Data_types.java

--- Data_types.java
+++ Data_types.java
@@ -4,6 +4,26 @@ class Datatypes{
         int integerValue = 42;
         System.out.println("Integer Value: " + integerValue);
 
+        // Long data type
+        long longValue = 123456789L;
+        System.out.println("Long Value: " + longValue);
+
+        // Short data type
+        short shortValue = 1000;
+        System.out.println("Short Value: " + shortValue);
+    
+        // String data type
+        String stringValue = "Hello, World!";
+        System.out.println("String Value: " + stringValue);
+
+        // Byte data type
+        byte byteValue = 127;
+        System.out.println("Byte Value: " + byteValue);
+
+        // Float data type
+        float floatValue = 3.14f;
+        System.out.println("Float Value: " + floatValue);
+
         // Double data type
         double doubleValue = 3.14;
         System.out.println("Double Value: " + doubleValue);
